30740e0ab8ef0b0cf68d0d9a3db1a468a46618be
charlie50503 <cate50503@gmail.com>
2022-06-18 21:17:25 +0900

    feat: 使用查詢參數 - 練習

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -12,7 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
-
+  public allowEdit = false;
   constructor(private serversService: ServersService,
     private route:ActivatedRoute) { }
 
@@ -30,7 +30,11 @@ export class EditServerComponent implements OnInit {
       this.serverName = this.server.name;
       this.serverStatus = this.server.status;
     })
+    this.route.queryParams.subscribe((queryParams:Params)=>{
+      console.log("queryParams",queryParams);
 
+      this.allowEdit = queryParams['allowEdit']==='1' ? true : false
+    })
     // const id = this.route.snapshot.paramMap.get('id'); // 取網誌參數寫法一
     // const id = this.route.snapshot.params['id']; // 取網誌參數寫法二
     const { id } = this.route.snapshot.params // 取網誌參數寫法三
