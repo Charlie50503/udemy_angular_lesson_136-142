7f52f16dfe2c151f7fad9aa98e7b0b76121e1d3f
charlie50503 <cate50503@gmail.com>
2022-06-18 20:18:13 +0900

    feat:  檢索查詢參數和片段

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -12,9 +13,19 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private route:ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.route.snapshot.queryParams) // 方法一
+    console.log(this.route.snapshot.fragment) // 方法一
+    this.route.fragment.subscribe((fragment)=>{
+      console.log("fragment",fragment);
+
+    })
+    this.route.queryParams.subscribe((params:Params)=>{
+      console.log("params",params);
+    })
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
