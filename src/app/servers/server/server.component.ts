c25c25312c10ec135f584f4c3a6b4aef6b8dfb04
charlie50503 <cate50503@gmail.com>
2022-06-18 21:06:35 +0900

    feat: 鑲嵌子路由

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -10,10 +11,19 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,private router:Router,
+    private route:ActivatedRoute) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
+    this.route.params.subscribe((params:Params)=>{
+      const id = +params['id'] | 1;
+      this.server = this.serversService.getServer(id);
+    })
+  }
+
+  onEdit(){
+    this.router.navigate(['edit'],{relativeTo:this.route})
   }
 
 }
