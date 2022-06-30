b95c31fbadcdd9d1d0c496bf48dfefd6d74c5725
charlie50503 <cate50503@gmail.com>
2022-06-30 16:51:05 +0900

    feat: angular-lesson-145 3.將router內容複製到app-routing.module.ts 並添加import和RouterModule

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,4 +1,35 @@
 import { NgModule } from "@angular/core";
+import { Routes,RouterModule } from "@angular/router";
+import { HomeComponent } from "src/app/home/home.component";
+import { PageNotFoundComponent } from "src/app/page-not-found/page-not-found.component";
+import { EditServerComponent } from "src/app/servers/edit-server/edit-server.component";
+import { ServerComponent } from "src/app/servers/server/server.component";
+import { ServersComponent } from "src/app/servers/servers.component";
+import { UserComponent } from "src/app/users/user/user.component";
+import { UsersComponent } from "src/app/users/users.component";
+
+
+const appRoutes: Routes = [
+  // { path: '', redirectTo: '/somewhere-else' ,pathMatch: 'full' }, 關於使用''會報錯的解法
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent ,children:[
+    { path: ':id/:name', component: UserComponent }
+  ]},
+  {
+    path: 'servers', component: ServersComponent,
+    children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent },
+    ]
+  },
+  {
+    path:'not-found',component:PageNotFoundComponent
+  },
+  {
+    path:'**',redirectTo:'/not-found'
+  },
+]
+
 
 @NgModule({})
 export class AppRoutingModule{
